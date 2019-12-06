c1630b29793d0c32cb667fb5346c79064f06a3e1
Victor <victor@github.com>
2019-12-07 02:41:08 +0530

    GenerateMessage Utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const publicPath = path.join(__dirname ,'../public');
 //console.log(publicPath);
+const {generateMessage} = require('./utils/message');
 const express = require('express');
 const port = process.env.PORT || 3000;
 const http = require('http');
@@ -18,27 +19,14 @@ io.on('connection', (socket) => {
     console.log('New User Connected');
 
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to ChatApp',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New User Joined',
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New User joined'));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', {
-            from : message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-
-        }); 
+        io.emit('newMessage', generateMessage(message.from,message.text)); 
 
 
         // socket.broadcast.emit('newMessage', {  //Emits to everyone except this one.
@@ -46,7 +34,7 @@ io.on('connection', (socket) => {
         //     text: message.text,
         //     createdAt: new Date().getTime()
         // });
-    })
+    });
 
 
     socket.on('disconnect',() => {
